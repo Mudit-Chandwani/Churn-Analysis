0c85b886499bfab6450e99ba9833167e2d1940ef
Mudit Chandwani <160005666+Mudit-Chandwani@users.noreply.github.com>
2024-11-11 21:09:43 +0530

    Add files via upload

---
+++ create View for Power BI.sql
@@ -0,0 +1,11 @@
+-- Create View for Power BI
+CREATE VIEW vw_ChurnData AS
+SELECT * 
+FROM prod_Churn 
+WHERE Customer_Status IN ('Churned', 'Stayed');
+
+CREATE VIEW vw_JoinData AS
+SELECT * 
+FROM prod_Churn 
+WHERE Customer_Status = 'Joined';
+
